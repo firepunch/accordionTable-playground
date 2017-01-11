a0b07f310be57a00fd9cc3d867b545a32ab9c640
firepunch <alfla456@gmail.com>
2017-01-11 23:59:39 +0900

    reactJs enviroment setup
    credit https://goo.gl/9a4OJl. 생 각보다 쉽게 설치해서 기분이 좋다.

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-$(function() {
-  var $list = $('.list');
-  $list.find("tr").not('.row').hide();
-  $list.find("tr").eq(0).show();
-  $list.find(".row").click(function() {
-    $(this).fadeTo("fast", 1);
-    $list.find('.row').not(this).siblings().fadeOut(500);
-    $(this).siblings().fadeToggle(500);
-    $(this).addClass('active');
-    $list.find('.row').not(this).removeClass('active');
-    $list.find('.row').not(this).hover(function() {
-        $(this).fadeTo("fast", 1);
-      },
-      function() {
-        $(this).fadeTo("fast", 0.33);
-      });
-  });
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
