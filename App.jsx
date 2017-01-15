98285b138af2b1a3907209754a12add06afc2709
firepunch <cynne13@naver.com>
2017-01-16 00:19:14 +0900

    insert data, css working

--- App.jsx
+++ App.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
-class App extends React.Component {
+class DataInsertTypeTable extends React.Component {
     render() {
-        return (
-            <div>
-                Hello World!!!
-            </div>
+        { /* comments */ }
+        const jobs = [];
+        const products = [];
+        const jobTypes = [ 'A', 'B', 'C', 'D' ];
+
+        function addJobs(quantity) {
+            const startId = jobs.length;
+            for (let i = 0; i < quantity; i++) {
+                const id = startId + i;
+                jobs.push({
+                    id: id,
+                    name: 'Item name ' + id,
+                    type: 'B',
+                    active: i % 2 === 0 ? 'Y' : 'N',
+                    datetime: '200' + i + '-12-28T14:57:00'
+                });
+            }
+        }
+
+        function addProducts(quantity) {
+            const startId = products.length;
+            for (let i = 0; i < quantity; i++) {
+                const id = startId + i;
+                products.push({
+                    id: id,
+                    name: 'Item name ' + id,
+                    price: 2100 + i
+                });
+            }
+        }
+
+        addJobs(5);
+        addProducts(5);
+
+        const cellEditProp = {
+            mode: 'click'
+        };
+
+        return  (
+            <BootstrapTable data={ jobs } cellEdit={ cellEditProp } insertRow={ true }>
+                <TableHeaderColumn isKey dataField='id'>Job ID</TableHeaderColumn>
+                <TableHeaderColumn dataField='name'>Job Name</TableHeaderColumn>
+                <TableHeaderColumn dataField='type' editable={ { type: 'select', options: { values: jobTypes } } }>Job Type</TableHeaderColumn>
+                <TableHeaderColumn dataField='active' editable={ { type: 'checkbox', options: { values: 'Y:N' } } }>Active</TableHeaderColumn>
+            </BootstrapTable>
         );
     }
 }
 
-export default App;
+export default DataInsertTypeTable;
--- main.js
+++ main.js
@@ -1,41 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App.jsx';
-// import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-
-// ReactDOM.render(<App />, document.getElementById('app'));
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-var ReactBsTable  = require('react-bootstrap-table');
-var BootstrapTable = ReactBsTable.BootstrapTable;
-var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
-
-const products = [];
-
-function addProducts(quantity) {
-    const startId = products.length;
-    for (let i = 0; i < quantity; i++) {
-        const id = startId + i;
-        products.push({
-            id: id,
-            name: 'Item name ' + id,
-            price: 2100 + i
-        });
-    }
-}
-
-addProducts(5);
-
-const cellEditProp = {
-    mode: 'click'
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DataInsertTypeTable from './App.jsx';
 
 ReactDOM.render(
-    <BootstrapTable data={ products } cellEdit={ cellEditProp }>
-        <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
-        <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-        <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
-    </BootstrapTable>,
+    <DataInsertTypeTable/>,
     document.getElementById('basic')
 );
